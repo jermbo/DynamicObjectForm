70a0e17bec0920d69da4e3632bed35884d699e3b
jlawson <jlawson@hugeinc.com>
2018-07-02 16:20:32 -0400

    cleaning up api and setting up options

--- src/scripts/DynamicObjectForm.js
+++ src/scripts/DynamicObjectForm.js
@@ -1,12 +1,18 @@
 console.clear();
-const DeepObjects = function (opts) {
-  const inputs = document.querySelectorAll(opts.inputs);
+
+const DeepObjects = function (options) {
+  const opts = {
+    textSeparator: '; ',
+    nameSplitter: '.'
+  }
+  const defaults = Object.assign(options, opts);
+  const inputs = document.querySelectorAll(defaults.inputs);
   let finalObj;
 
-  function getFormValues() {
+  function formToJSON() {
     finalObj = {};
     inputs.forEach(input => {
-      const keys = input.name.split(".");
+      const keys = input.name.split(defaults.nameSplitter);
       const lastKey = keys.pop();
       const lastObj = keys.reduce(
         (obj, key) => (obj[key] = obj[key] || {}),
@@ -27,7 +33,7 @@ const DeepObjects = function (opts) {
       }
 
       if (input.type == "textarea") {
-        lastObj[lastKey] = input.value.split("; ");
+        lastObj[lastKey] = input.value ? input.value.split(defaults.textSeparator) : [];
         return;
       }
 
@@ -37,27 +43,9 @@ const DeepObjects = function (opts) {
     return finalObj;
   }
 
-  function getValues() {
-
-  }
-
-  function uploadValues(opts) {
-    if (!opts.url) throw new Error('Need some place to put the data');
-    if (!opts.data) throw new Error('Need to know what to save');
-    fetch(opts.url, {
-      method: 'POST',
-      body: JSON.stringify(opts.data),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(resp => console.log(resp))
-      .catch(err => console.log(err));
-  }
-
-  function displayValues(data) {
+  function jsonToForm(data) {
     inputs.forEach(input => {
-      const keys = input.name.split(".");
+      const keys = input.name.split(defaults.nameSplitter);
       const value = keys.reduce(
         (prev, curr) => (prev ? prev[curr] : null),
         data
@@ -98,11 +86,20 @@ const DeepObjects = function (opts) {
     });
   }
 
+  function resetForm() {
+    inputs.forEach(input => {
+      if (input.type == "checkbox" || input.type == "radio") {
+        input.checked = false;
+        return;
+      }
+      input.value = "";
+    });
+  }
+
   return {
-    uploadValues: uploadValues,
-    getValues: getValues,
-    getFormValues: getFormValues,
-    displayValues: displayValues
+    formToJSON: formToJSON,
+    jsonToForm: jsonToForm,
+    resetForm: resetForm
   };
 };
 
